f04803477ba08f08e5cb72ebed781bb8da30f1c6
Raheem8943 <Raheem318558@gmail.com>
2025-05-14 22:24:25 +0530

    Add files via upload

---
+++ task_portfolio.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+</footer>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Resume</title>
+	<style>
+body{
+   
+   font-family: Arial , sans-serif;
+}
+
+
+
+h1{
+
+	color:navy blue;
+	font-family: verdana;
+	font-size: 300%;
+}
+div{
+   
+	border: 1px solid black;
+    margin-bottom: 5px;
+    margin-left: 300px;
+    margin-right: 300px;
+	margin-top: 50px;
+	padding-right: 7px;
+	padding-left: 10px;
+	padding-top: 7px;
+	padding-bottom: 7px;
+}
+
+nav{
+
+}
+table, th, td{
+   border: 1px solid black;
+}
+
+	</style>
+</head>
+<body>
+	<div>
+	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsTw9D7W7sAHUZYLyE7M4ELBiZsJ-CYFtQkoKOL-bbOt6h4ZMbJNjmREPBLP9yFbIuqxI&usqp=CAU" alt="profile">
+	<h2>ALEX BROWN </h2>
+
+	<p>
+		<b>Objective</b><br>
+    Committed and goal-oriented computer science graduate looking to pursue a career in the software 
+engineering domain. Possess excellent knowledge in Java, C, C++ and strong knowledge in database 
+such as MySQL. Ability to learn things quickly and capable of working in a fast-paced and team-driven 
+environment.</p>
+<nav>
+<ul>
+   <li><a href="#projects"></a>projects</li>
+   <li><a href="contact"></a>contact</li>
+   <li><a href="about"></a> about</li>
+</ul>
+</nav>
+
+
+     
+
+     <section id="projects">
+      <h3>projects</h3>
+      <ul>
+         <li>Library management system in java</li>
+          <li>portfolio website using HTML</li>
+          <li>company database using mysql</li>
+
+
+      </ul>
+      
+
+   </section>
+      <section id="contact">
+      	<h3>contact</h3>
+         <p>Email:alex@gmail.com</p>
+         <p>Linkedin: Linkedin.com/alex421</p>
+      </section>
+
+   <section id="about">
+
+       <h3>about</h3>
+      <p>I am a recent graduate in Computer Science with a strong interest in building software solutions. I enjoy learning new technologies and solving problems efficiently.</p>
+      <dl>
+        <td>My hobbies are:</td>
+        <dd>-Reading</dd>
+        <dd>-dance</dd>
+        <dd>-photography</dd>
+          
+      </dl>
+   </section>
+
+   <table style="width: 100%;">
+         <tr>
+         <th style="width:50%">Programing Languages</th>
+         <th style="width:50%">Tools</th>
+
+
+         </tr>
+         <td>
+            <ul>
+             <li>java</li>
+             <li>C</li>
+            <li>C++</li>
+            <li>PHP</li>
+            <li>JavaScript</li>
+            <li>Python</li>
+
+            </ul>
+            </td>
+
+
+
+             <td>
+            <ul>
+             <li>Visual  Studio Code</li>
+             <li>jira</li>
+            <li>Github</li>
+            <li>Sublime Text</li>
+            <li>Eclips</li>
+            <li>Docker</li>
+
+            </ul>
+            </td>
+            </table>
+
+   <h3>Projects</h3>
+   <dl>
+      <td> <ins><b>Library management system:- </td></ins></b>
+      <dd> A Java-based desktop application for managing book records, users, and transactions.</dd>
+      <dd><a href="https://github.com/Alex/library-project" target="_blank" rel="noopenernoreferrer">https://github.com/Alex/library-project</a> </dd>
+   </dl>
+
+   <dl>
+      <td> <ins><b>Portfolio Website:- </td></ins></b>
+      <dd> A personal portfolio website built using HTML, CSS, and JavaScript to showcase projects and skills.</dd>
+      <dd><a href="https://github.com/alex/portfolio" target="_blank" rel="noopenernoreferrer">https://github.com/alex/portfolio</a> </dd>
+   </dl>
+
+   <dl>
+      <td> <ins><b>company database:- </td></ins></b>
+      <dd> Designed and implemented a relational database for managing employee records, departments, and salaries using MySQL. Includes queries, joins, and stored procedures. </dd>
+      <dd><a href="https://github.com/alex/portfolio" target="_blank" rel="noopenernoreferrer">https://github.com/alex/Company_db</a> </dd>
+   </dl>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
+<footer style="text-align: center;margin-top:40px ;">
+   <p>&copy;This website created by "Raheem"</p>
+
+
+
+</html>
